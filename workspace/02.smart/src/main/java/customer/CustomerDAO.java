69ed9d426dca750442fe068ebfddfd530846a011
Kwakyoung <128439473+Kwakyoung@users.noreply.github.com>
2023-06-07 17:52:35 +0900

    0607

--- workspace/02.smart/src/main/java/customer/CustomerDAO.java
+++ workspace/02.smart/src/main/java/customer/CustomerDAO.java
@@ -4,26 +4,29 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 @Repository   
 public class CustomerDAO implements CustomerService{
 
-	private SqlSession sql;
+	@Autowired private SqlSession sql;
 	
 	// 생성된 객체 (bean으로 등록된 객체들)의 주소를 스프링 container에 관리가 됨.
 	// IoC (Inversion of Control) 개발자가 필요할 때마다 new 라는 키워드로 객체를 생성 -> 스프링프레임웤이 객체를 생성
 	
 	// DI (Dependency Injection) 객체의 주소를 담아주는(주입) 처리 
 	// 필드에 데이터 담는 방법 2가지
-	public CustomerDAO(SqlSession sql) {
-		this.sql = sql;
-	}
+//	public CustomerDAO(SqlSession sql) {
+//		this.sql = sql;
+//	}
+	
+	// executQuery, executeUpdate
 	
 	@Override
 	public void customer_insert(CustomerVO vo) {
-		
+		sql.insert("customer.insert", vo);
 		
 	}
 
@@ -36,19 +39,24 @@ public class CustomerDAO implements CustomerService{
 	@Override
 	public CustomerVO customer_info(int id) {
 		
-		return null;
+		return sql.selectOne("customer.info", id);
 	}
 
 	@Override
 	public void customer_update(CustomerVO vo) {
-		
+		sql.update("customer.update", vo);
 		
 	}
 
 	@Override
 	public void customer_delete(int id) {
+		sql.delete("customer.delete", id);
 		
-		
+	}
+
+	@Override
+	public List<CustomerVO> customer_list(String name) {
+		return sql.selectList("customer.list", name);
 	}
 	
 }
--- workspace/02.smart/src/main/java/customer/CustomerServiceImpl.java
+++ workspace/02.smart/src/main/java/customer/CustomerServiceImpl.java
@@ -13,7 +13,7 @@ public class CustomerServiceImpl implements CustomerService {
 	
 	@Override
 	public void customer_insert(CustomerVO vo) {
-		
+		dao.customer_insert(vo);
 
 	}
 
@@ -24,20 +24,25 @@ public class CustomerServiceImpl implements CustomerService {
 
 	@Override
 	public CustomerVO customer_info(int id) {
-		
-		return null;
+		return dao.customer_info(id);
 	}
 
 	@Override
 	public void customer_update(CustomerVO vo) {
-		
+		dao.customer_update(vo);
 
 	}
 
 	@Override
 	public void customer_delete(int id) {
-		
+		dao.customer_delete(id);
 
+	}
+
+	@Override
+	public List<CustomerVO> customer_list(String name) {
+		
+		return dao.customer_list(name);
 	}
 
 }
